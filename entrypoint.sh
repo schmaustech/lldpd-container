3559daac42ee2554dcf2dfa704d8cf3efd6c6ba6
Ben Schmaus <schmaustech@yahoo.com>
2025-10-01 16:15:17 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
-# Set working dir
-cd /root
 
-# Determine all the Mellanox interfaces to enable lldpd on
+# Gather Ethernet devices that are Mellanox
+mapfile -t devices < <( lspci -nn | awk '/Ethernet/ && /Mellanox/ {print $1}' )
 
-# Start lldpd and set to Mellanox interfaces
+# Iterate through the devices and find the real interface name and add it to interfaces string
+for i in "${devices[@]}"
+do
+  itmp=`grep PCI_SLOT_NAME /sys/class/net/*/device/uevent|grep $i|awk -F'/' {'print $5'}`
+  interfaces="${interfaces} ${itmp}"
+done
 
-
+# Run lldpd daemon passing the interfaces for it to run on
+/usr/sbin/lldpd -dd -l -C $interfaces
 
 # Echo to logs container is ready - Sleep container indefinitly
 
 echo "-------------------------------------------------------------------"
-echo "All components for the container have been Installed!"
-echo "Testing and tool usage is Ready!"
+echo "LLDPD is up and running on $interfaces!"
 echo "-------------------------------------------------------------------"
 sleep infinity & wait
