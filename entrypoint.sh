b427faa2372eccf23450e344ca574bc2653b2f65
Ben Schmaus <schmaustech@yahoo.com>
2025-10-01 15:26:48 -0500

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Set working dir
+cd /root
+
+# Determine all the Mellanox interfaces to enable lldpd on
+
+# Start lldpd and set to Mellanox interfaces
+
+
+
+# Echo to logs container is ready - Sleep container indefinitly
+
+echo "-------------------------------------------------------------------"
+echo "All components for the container have been Installed!"
+echo "Testing and tool usage is Ready!"
+echo "-------------------------------------------------------------------"
+sleep infinity & wait
